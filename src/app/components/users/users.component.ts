a79e7a8699a6d83eaca1ba8215f8e004e7b5df2e
Pedro Castro <pedro.tiago.castro.ist@gmail.com>
2019-01-18 16:50:35 +0000

    Section 6 - Working with Observables - exemplo com setTimeout assíncrono

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -21,10 +21,14 @@ export class UsersComponent implements OnInit {
   currentStyles = {};
   showUserForm: boolean = false;
   @ViewChild('userForm') form: any;
+  data: any;
 
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
+    this.dataService.getData().subscribe(data => {
+      console.log(data);
+    })
 
     this.users = this.dataService.getUsers();
 
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs'
 
 import { User } from '../models/User';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
   users: User[];
+  data: Observable<any>;
 
   constructor() {
     this.users = [
@@ -40,7 +43,29 @@ export class DataService {
     ];
   }
 
+  getData() {
+    this.data = new Observable(observer => {
+      setTimeout(() => {
+        observer.next(1);
+      }, 1000);
+
+      setTimeout(() => {
+        observer.next(2);
+      }, 2000);
+
+      setTimeout(() => {
+        observer.next(3);
+      }, 3000);
+
+      setTimeout(() => {
+        observer.next(4);
+      }, 4000);
+    });
+    return this.data;
+  }
+
   getUsers(): User[] {
+    console.log("Fetching users from service...");
     return this.users;
   }
 
