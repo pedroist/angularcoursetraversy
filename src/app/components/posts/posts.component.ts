ddb1c4844000524550eb368cfb892bef6adb5394
Pedro Castro <pedro.tiago.castro.ist@gmail.com>
2019-01-22 16:58:15 +0000

    Section 6 - PUT and DELETE

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -33,4 +33,32 @@ export class PostsComponent implements OnInit {
     this.currentPost = post;
     this.isEdit = true;
   }
+
+  onUpdatedPost(post: Post) {
+    this.posts.forEach((cur, index) => {
+      if (post.id === cur.id) {
+        this.posts.splice(index, 1); //remove o post antigo que foi updated
+        this.posts.unshift(post); //Insere o post updated no inicio do array
+        this.isEdit = false;
+      }
+      this.currentPost = {
+        id: 0,
+        title: '',
+        body: ''
+      }
+    });
+  }
+
+  removePost(post: Post) {
+    if (confirm('Are you sure?')) {
+      this.postService.removePost(post.id).subscribe(() => {
+        this.posts.forEach((cur, index) => {
+          if (post.id === cur.id) {
+            this.posts.splice(index, 1); //remove o post antigo que foi updated
+          }
+        });
+      });
+    }
+  }
+
 }
